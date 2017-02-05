3cc0c753ff46ceae33fff42987a9869699fb2344
Machi <matchimar.worrawattanaphat@gmail.com>
2017-02-05 14:31:09 -0500

    Assignment Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This fuction is to calculate the inverse of the matrix. If it's called for 2nd time it will getting 
+## the cache data instead of recaluate the function again.
+##-------------------------------------------------------------------
+## This Fuction will create the list of the functions that store the assigned data. 
+##-------------------------------------------------------------------
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This fuction will calcualte the inverse of a matrix and return the cache value if it's called for the 2nd time. 
+##-------------------------------------------------------------------
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
